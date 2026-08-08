0f62d8212f28674c28ce450db4952525c3dd0101
Bernardo <contato.bernardoar@gmail.com>
2026-08-08 20:18:32 -0300

    Added the basics for contract and project

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,5 +1,55 @@
+import { useTranslation } from "react-i18next";
+
+type ContactData = {
+  title: string;
+  value: string;
+  href: string;
+};
 const Contact = () => {
-  return <div>Contact</div>;
+  const { t } = useTranslation();
+  const contacts: ContactData[] = [
+    {
+      title: "Email",
+      value: "contato.bernardoar@gmail.com",
+      href: "mailto:contato.bernardoar@gmail.com",
+    },
+    {
+      title: "LinkedIn",
+      value: "bernardoroballo",
+      href: "https://linkedin.com/in/bernardoroballo",
+    },
+    {
+      title: "GitHub",
+      value: "BernardoAR",
+      href: "https://github.com/BernardoAR",
+    },
+  ];
+
+  return (
+    <div className="grid items-center text-white p-10 space-y-4">
+      <div className="text-center">
+        <h2 className="text-5xl">{t("contact.title")}</h2>
+        <h3 className="text-white/40 text-2xl">{t("contact.subtitle")}</h3>
+      </div>
+      {contacts.map((value, idx) => (
+        <div
+          key={idx}
+          className="rounded-xl border border-white/10 bg-gray-900 p-6 space-y-4 w-full lg:w-5/12 mx-auto"
+        >
+          <div className="flex flex-col text-sm justify-between">
+            <div>
+              <div className="font-bold text-white/70">{value.title}</div>
+            </div>
+            <div className="text-sm">
+              <a className="underline" href={value.href} target="_blank">
+                {value.value}
+              </a>
+            </div>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default Contact;
--- src/pages/Project.tsx
+++ src/pages/Project.tsx
@@ -1,5 +1,15 @@
+import { useTranslation } from "react-i18next";
+
 const Project = () => {
-  return <div>Project</div>;
+  const { t } = useTranslation();
+  return (
+    <div className="grid items-center text-white p-10 space-y-4">
+      <div className="text-center">
+        <h2 className="text-5xl">{t("project.title")}</h2>
+        <h3 className="text-white/40 text-2xl">{t("project.subtitle")}</h3>
+      </div>
+    </div>
+  );
 };
 
 export default Project;
